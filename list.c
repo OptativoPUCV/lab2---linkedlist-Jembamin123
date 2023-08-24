4309d9cf7f545ee5bb17ae3d2f2c961ce2a8c53c
Replit user <>
2023-08-24 19:00:50 +0000

    new try

--- list.c
+++ list.c
@@ -55,7 +55,7 @@ void * nextList(List * list) {
 
 void * firstList(List * list) {
   if (list->head !=NULL){
-    list->current =list->heat;
+    list->current =list->head;
     return list->current->data;
   }
   else{
